878528972e95d3ba64827b26af5b9f2f1969160f
Josue <josuee.ald@gmail.com>
2018-05-23 20:27:29 -0500

    irror

--- core/modules/index/view/home/widget-default.php
+++ core/modules/index/view/home/widget-default.php
@@ -8,4 +8,26 @@
 	<h1>Inicio</h1>
 	<p>Bienvenido a la torre.</p>
 	</div>
+	<div class="container">
+	<div class="col-md-12">
+	<div class="container-fluid">
+
+	    <div class="row">
+	      <div class="col-lg-4"  style="background-color:#ccc"">
+	        <h1>Tardanzas</h1> 
+	        <p>#alumnos: 12</p>
+	      </div>
+	      <div class="col-lg-4"  style="background-color:#bbb">
+	        <h1>Faltas</h1> 
+	        <p>#alumnos: 4.</p>
+	      </div>
+	      <div class="col-lg-4"  style="background-color:#ccc">
+	        <h1>Hot news</h1> 
+	        <p>Cepu otoño: S/. 100.00 x alumno :v</p>
+	      </div>
+    	</div>
+	</div>
+
+  </div> 
+</div>
 </div>
