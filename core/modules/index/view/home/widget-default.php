df78dd3164c5ba61f384043b5553338af6852255
converse98 <leandroandre1538@gmail.com>
2018-07-04 19:10:53 -0500

    f5 bugs

--- core/modules/index/view/home/widget-default.php
+++ core/modules/index/view/home/widget-default.php
@@ -25,7 +25,7 @@
 	      
 	     
 	      <div class="col-md-4" align="center" 
-	        class="grid-block" style="background-image: url('canal1.jpg');  width:160px; height:168px; min-width: 160px ; max-width: 160px; min-height: 168px; max-height: 168px; display:block; margin-left: 45px; border-style: solid; border-width:1px;  border-color:#000000"> 
+	        class="grid-block" style="background-image: url('notsirve/canal1.jpg');  width:160px; height:168px; min-width: 160px ; max-width: 160px; min-height: 168px; max-height: 168px; display:block; margin-left: 45px; border-style: solid; border-width:1px;  border-color:#000000"> 
 	      </div>
 
 	      <div class="col-md-2" align="center" class="grid-block" style="background-color:#212121;width:250px; height:110px;    max-width: 300px; min-height: 100px; min-width: 50px; ">
@@ -40,7 +40,7 @@
 		
 
 	      <div class="col-md-2" align="center" 
-	         class="grid-block" style="background-image: url('canal2.jpg'); width:160px; height:168px; min-width: 140px ; max-width: 160px; min-height: 168px; max-height: 168px; margin-left: 45px; border-style: solid; border-width:1px;  border-color:#000000">
+	         class="grid-block" style="background-image: url('notsirve/canal2.jpg'); width:160px; height:168px; min-width: 140px ; max-width: 160px; min-height: 168px; max-height: 168px; margin-left: 45px; border-style: solid; border-width:1px;  border-color:#000000">
 	      </div>
 	      
 	      <div class="col-md-3"  style="background-color:#212121; width:250px; height:110px;max-width: 300px; min-height: 100px; min-width: 10px; min-width: 50px; ">
@@ -62,7 +62,7 @@
 	      
 	     
 	      <div class="col-md-4" align="center" 
-	        class="grid-block" style="background-image: url('canal3.jpg');  width:160px; height:168px; min-width: 160px ; max-width: 160px; min-height: 168px; max-height: 168px; display:block; margin-left: 45px;border-style: solid; border-width:1px;  border-color:#000000"> 
+	        class="grid-block" style="background-image: url('notsirve/canal3.jpg');  width:160px; height:168px; min-width: 160px ; max-width: 160px; min-height: 168px; max-height: 168px; display:block; margin-left: 45px;border-style: solid; border-width:1px;  border-color:#000000"> 
 	      </div>
 
 	      <div class="col-md-2" align="center" class="grid-block" style="background-color:#212121;width:250px; height:110px;    max-width: 300px; min-height: 100px; min-width: 50px; ">
@@ -77,7 +77,7 @@
 		
 
 	      <div class="col-md-2" align="center" 
-	         class="grid-block" style="background-image: url('canal4.jpg'); width:160px; height:168px; min-width: 160px ; max-width: 160px; min-height: 168px; max-height: 168px; margin-left: 45px;border-style: solid; border-width:1px;  border-color:#000000">
+	         class="grid-block" style="background-image: url('notsirve/canal4.jpg'); width:160px; height:168px; min-width: 160px ; max-width: 160px; min-height: 168px; max-height: 168px; margin-left: 45px;border-style: solid; border-width:1px;  border-color:#000000">
 	      </div>
 	      
 	      <div class="col-md-3"  style="background-color:#212121; width:250px; height:110px;max-width: 300px; min-height: 100px; min-width: 10px; min-width: 50px; ">
